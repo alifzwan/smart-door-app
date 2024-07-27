c9ec7265f820cfd4dc4f608272f3cfc0494e58fc
alifzwan <alifzakwan529@gmail.com>
2024-07-27 20:24:29 +0800

    refactor: Remove unused StyleSheet import in Main.jsx

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
 import SignIn from './components/signin/SignIn'
 import { Routes, Route, Navigate } from 'react-router-native'
@@ -6,24 +6,20 @@ import Room from './components/room/Room'
 import Lock from './components/lock/Lock'
 import AppBar from './components/bar/AppBar'
 import { RoomProvider } from './utils/RoomContext'
+
 const Main = () => {
   return (
     <RoomProvider>
       <View>
-          <Routes>
-            <Route path="/" element={<SignIn />} />
-            <Route path="/room" element={<Room />} />
-            <Route path="/lock" element={<><AppBar /><Lock /></>} />
-            <Route path="*" element={<Navigate to="/" replace/>}/>
-          </Routes>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/room" element={<Room />} />
+          <Route path="/lock" element={<><AppBar /><Lock /></>} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </View>
     </RoomProvider>
   )
 }
 
 export default Main
-
-const styles = StyleSheet.create({
-    container:{
-    }
-})
