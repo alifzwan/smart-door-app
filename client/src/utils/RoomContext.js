a91f37f00281296c9c0a76d14cbdec9b68732c9e
alifzwan <alifzakwan529@gmail.com>
2024-07-29 16:34:50 +0800

    refactor: Add studentId state to RoomContext
    This commit adds the `studentId` state to the `RoomContext` component in `RoomContext.js`. The `studentId` state is used to store the ID of the current student. This change enhances the functionality of the application by allowing the student ID to be accessed and updated within the `RoomContext` component.

--- client/src/utils/RoomContext.js
+++ client/src/utils/RoomContext.js
@@ -1,13 +1,16 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 export const RoomContext = createContext();
 
 export const RoomProvider = ({ children }) => {
     const [selectedRoom, setSelectedRoom] = useState(null);
+    const [studentId, setStudentId] = useState(null)
 
     return (
-        <RoomContext.Provider value={{ selectedRoom, setSelectedRoom }}>
+        <RoomContext.Provider value={{ selectedRoom, setSelectedRoom, studentId, setStudentId }}>
             {children}
         </RoomContext.Provider>
     );
 };
+
+export const useRoomContext = () => useContext(RoomContext)
