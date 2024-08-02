faf85c7c743f9f1f43a170d9a07c56ce4826f634
alifzwan <alifzakwan529@gmail.com>
2024-08-02 15:31:08 +0800

    refactor: Improve DateBar component
    This commit improves the DateBar component in DateBar.jsx. It updates the styling and layout of the component, including centering the content horizontally and adding vertical padding for better spacing. The back button is positioned on the left side with some margin, and the text "Select Date" is centered vertically with the arrow icon. This change enhances the user interface of the application by providing a more visually appealing and user-friendly date selection bar.

--- client/src/components/bar/DateBar.jsx
+++ client/src/components/bar/DateBar.jsx
@@ -1,37 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, Pressable, View } from 'react-native'
 import React from 'react'
+import AntDesign from '@expo/vector-icons/AntDesign'
+import { useNavigate, Link } from 'react-router-native'
+import * as theme from '../../theme/theme'
 
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center',
-        margin: 10,
-        paddingBottom: 10,
+        justifyContent: 'center', // Center the content horizontally
+        paddingVertical: 10, // Add some vertical padding for better spacing
+        position: 'relative', // Needed for the absolute positioning of the back button
     },
     backButton: {
+      position: 'absolute', // Position it absolutely within the container
+      left: 10, // Position it on the left side with some margin
       flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center'
+      alignItems: 'center', // Center the arrow icon vertically with the text
     },
     backText: {
       marginLeft: 5,
       fontSize: theme.fontSize.subheading,
       fontWeight: theme.fontWeights.bold,
+      textAlign: 'center',
+      fontFamily: 'Helvetica-World'
     }
 })
 
 const DateBar = () => {
-    const navigate = useNavigate()
-    return (
-      <View style={styles.container}>
-        <Pressable onPress={() => navigate(-1)} style={styles.backButton}>
-          <AntDesign name="arrowleft" size={20} color="black" />
-          <Text style={styles.backText}>Select Date</Text>
-        </Pressable>
-      </View>
-    )
+  const navigate = useNavigate()
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={() => navigate(-1)} style={styles.backButton}>
+        <AntDesign name="arrowleft" size={20} color="black" />
+      </Pressable>
+
+      <Text style={styles.backText}>Select Date</Text>
+    </View>
+  )
 }
 
 export default DateBar
-
