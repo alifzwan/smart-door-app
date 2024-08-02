90ad9d9de9d59af16856f88918c946986cab045d
alifzwan <alifzakwan529@gmail.com>
2024-08-02 15:31:25 +0800

    refactor: Add Homepage, Book, and DateBar components
    This commit adds the Homepage, Book, and DateBar components to the application. The Homepage component improves the styling and layout of the homepage, enhancing the user interface. The Book component adds booking functionality with a header, room list, and booking buttons. The DateBar component provides a dedicated date selection bar. These changes enhance the functionality and user experience of the application.

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -4,7 +4,10 @@ import SignIn from './components/signin/SignIn'
 import { Routes, Route, Navigate } from 'react-router-native'
 import Room from './components/room/Room'
 import Lock from './components/lock/Lock'
+import Homepage from './components/homepage/Homepage'
+import Book from './components/book/Book'
 import AppBar from './components/bar/AppBar'
+import DateBar from './components/bar/DateBar'
 import { RoomProvider } from './utils/RoomContext'
 
 const Main = () => {
@@ -13,7 +16,9 @@ const Main = () => {
       <View>
         <Routes>
           <Route path="/" element={<SignIn />} />
-          <Route path="/room" element={<Room />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/room" element={<><AppBar /><Room /></>} />
+          <Route path="/book" element={<><DateBar /><Book /></>} />
           <Route path="/lock" element={<><AppBar /><Lock /></>} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
