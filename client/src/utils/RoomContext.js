60fe46608c82dc017eea5918c6a600b7d69f3f29
alifzwan <alifzakwan529@gmail.com>
2024-08-03 18:44:16 +0800

    refactor: Add selectedDate, selectedTimeSlot, and bookedSlots to RoomProvider
    This commit adds the selectedDate, selectedTimeSlot, and bookedSlots states to the RoomProvider component in RoomContext.js. These states are necessary for implementing the room booking functionality, allowing users to select a date, time slot, and view the booked slots. This change enhances the functionality and user experience of the application.

--- client/src/utils/RoomContext.js
+++ client/src/utils/RoomContext.js
@@ -3,11 +3,28 @@ import React, { createContext, useContext, useState } from 'react';
 export const RoomContext = createContext();
 
 export const RoomProvider = ({ children }) => {
-    const [selectedRoom, setSelectedRoom] = useState(null);
+    const [selectedRoom, setSelectedRoom] = useState(null)
     const [studentId, setStudentId] = useState(null)
+    const [selectedDate, setSelectedDate] = useState(null)
+    const [selectedTimeSlot, setSelectedTimeSlot] = useState(null)
+    const [bookedSlots, setBookedSlots] = useState({})
+
 
     return (
-        <RoomContext.Provider value={{ selectedRoom, setSelectedRoom, studentId, setStudentId }}>
+        <RoomContext.Provider 
+            value={{ 
+                selectedRoom,
+                setSelectedRoom,
+                studentId,
+                setStudentId,
+                selectedDate,
+                setSelectedDate,
+                selectedTimeSlot,
+                setSelectedTimeSlot,
+                bookedSlots,
+                setBookedSlots 
+            }}
+        >
             {children}
         </RoomContext.Provider>
     );
