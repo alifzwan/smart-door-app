ed32f4ec8e83dc53ee70f7f296f9dd7cea4fa067
alifzwan <alifzakwan529@gmail.com>
2024-08-05 16:18:28 +0800

    refactor: Add lazy loading for images in Homepage component

--- client/src/components/homepage/Homepage.jsx
+++ client/src/components/homepage/Homepage.jsx
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View, Pressable, Image } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Pressable, Image, ActivityIndicator  } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import { Ionicons, FontAwesome } from '@expo/vector-icons'
 import { useNavigate } from 'react-router-native'
 import theme from '../../theme/theme'
 import { LinearGradient } from 'expo-linear-gradient'
+import { useRoomContext } from '../../utils/RoomContext'
+import { supabase } from '../../lib/supabase'
 
 
 const styles = StyleSheet.create({
@@ -81,10 +83,40 @@ const styles = StyleSheet.create({
 
 const Homepage = () => {
   const navigate = useNavigate()
+  const { studentId } = useRoomContext()
+
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    if(studentId) {
+      fetchName()
+    }
+    
+  }, [studentId])
+
+  const fetchName = async () => {
+    try {
+      const { data, error } = await supabase
+        .from('students')
+        .select('name')
+        .eq('student_id', studentId)
+        .single()
+
+      if(error){
+        throw error
+      }
+
+      setName(data.name)
+
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
 
   const handlePress = () => {
     navigate('/room')
   }
+
 
 
   return (
@@ -92,7 +124,11 @@ const Homepage = () => {
       <LinearGradient colors={['#FF512F', '#DD2476']} style={styles.header}>
         <View style={styles.userInfo}>
             <FontAwesome name="user-circle" size={30} color="white" style={{marginRight: 5}}/>
-            <Text style={styles.welcomeText}>Hi, Alif Zakwan</Text>
+            {name ? (
+              <Text style={styles.welcomeText}>{name}</Text>
+            ) : (
+              <ActivityIndicator size="small" color="#fff" />
+            )}
         </View>
       </LinearGradient>
 
